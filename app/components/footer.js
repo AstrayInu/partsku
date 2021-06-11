5277a3699f76b7e82b9c1196910c3d7392649306
Fritz Immanuel <v.fritz.immanuel@adira.co.id>
2021-06-11 21:37:10 +0700

    add footer (needs rework)

---
+++ app/components/footer.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import { later } from '@ember/runloop';
+import { set } from '@ember/object';
+
+export default class FooterComponent extends Component {
+
+}
